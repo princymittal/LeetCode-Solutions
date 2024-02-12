3e9e12788e290dfc48865ec0def52dde65ee899b
princymittal <76686442+princymittal@users.noreply.github.com>
2024-02-12 20:14:01 +0530

    Time: 5 ms (31.09%), Space: 45.4 MB (23.38%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int missingNumber(int[] nums) {
         Arrays.sort(nums);
         
@@ -8,5 +8,18 @@ class Solution {
               
        } 
        return nums.length; 
+    }
+}*/ 
+class Solution {
+    public int missingNumber(int[] nums) {
+        int c = 0; 
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length  ; i++) {
+            if (nums[i]==c) {
+                c++;
+            }
+            else return c ; 
+        }
+        return c ; 
     }
 }
