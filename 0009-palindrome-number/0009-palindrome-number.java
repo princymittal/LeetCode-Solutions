3b85f0e8e8397a96beb333189372583e8f90d1de
princymittal <76686442+princymittal@users.noreply.github.com>
2024-02-16 09:10:03 +0530

    Time: 4 ms (100.00%), Space: 44 MB (54.84%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public boolean isPalindrome(int x) {
         if(x<0) return false;
         int s=0;
@@ -10,6 +10,20 @@ class Solution {
         }
         return(x==s);
 
+        
+    }
+}*/
+// another logic
+class Solution{
+    public boolean isPalindrome(int x){
+        if(x<0 || (x%10==0 && x!=0)){
+            return false;}
+        int reverse=0;
+            while(x>reverse){
+                reverse=reverse*10+x%10;
+                x/=10;
+            }
+            return x==reverse || x== reverse/10;
         
     }
 }
